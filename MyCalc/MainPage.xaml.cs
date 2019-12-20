ac62503da5127d6c6ab4c8f0d0c9137f9e811891
Сергей Василевский <sarge14@mail.ru>
2019-12-20 07:50:30 +0300

    0.1.3

--- MyCalc/MainPage.xaml.cs
+++ MyCalc/MainPage.xaml.cs
@@ -22,62 +22,91 @@ namespace MyCalc
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        float a, b;
-        int count;
-        bool znakmin = false;
-
+        float a;
+        int count = 0;
+        bool znak = false;
 
         public MainPage()
         {
             this.InitializeComponent();
             sumText.Text = "0";
-            numText.Text = "0";
+            numText.Text = "";
         }
         private void CalculateSum()
         {
             switch (count)
             {
                 case 1:
-                    b = a + float.Parse(sumText.Text);
-                    sumText.Text = b.ToString("F8");
+                    a += float.Parse(sumText.Text);
+                    sumText.Text = a.ToString("F8");
                     break;
                 case 2:
-                    b = a - float.Parse(sumText.Text);
-                    sumText.Text = b.ToString("F8");
+                    a -= float.Parse(sumText.Text);
+                    sumText.Text = a.ToString("F8");
                     break;
                 case 3:
-                    b = a * float.Parse(sumText.Text);
-                    sumText.Text = b.ToString("F8");
+                    a *= float.Parse(sumText.Text);
+                    sumText.Text = a.ToString("F8");
                     break;
                 case 4:
                     float divider;
                     divider = float.Parse(sumText.Text);
                     if (divider == 0.0)
-                        sumText.Text ="Деление на ноль";
+                        numText.Text = "Деление на ноль";
                     else
                     {
-                        b = a / divider;
-                        sumText.Text = b.ToString();
+                        a /= divider;
+                        sumText.Text = a.ToString("F8");
                     }
-                        break;
+                    break;
 
                 default:
                     break;
             }
-
+            int lenght = sumText.Text.Length -1;
+            string text = sumText.Text;
+            //  sumText.Text = "";
+            for (int i = lenght; i > 0; --i)
+            {
+                if (text[i].ToString() == "0" || text[i].ToString() == ",")
+                    sumText.Text = sumText.Text.Remove(i, 1);
+                else break;
+            }
         }
+
         private void Sum_Click(object sender)
         {
-            numText.Text = sumText.Text + (sender as Button).Content;
-            sumText.Text = "0";
+            znak = true;
+            int lenght = numText.Text.Length - 1;
+            string text = numText.Text;
+            if (numText.Text.IndexOf((sender as Button).Content.ToString()) == -1)
+            {
+                numText.Text = sumText.Text + (sender as Button).Content;
+            }
+            else
+            { 
+                numText.Text = "";
+                for (int i = 0; i < lenght; i++)
+                {
+                    numText.Text += text[i];
+                }
+                numText.Text = a.ToString() + (sender as Button).Content;
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (sumText.Text =="0")
-                sumText.Text =  (sender as Button).Content.ToString();
+            
+            if (sumText.Text == "0" || numText.Text == "Деление на ноль" || znak)
+            {
+                sumText.Text = (sender as Button).Content.ToString();
+                znak = false;
+            }
             else
                 sumText.Text += (sender as Button).Content;
+
+            if (numText.Text == "Деление на ноль")
+                numText.Text = "";
         }
 
 
@@ -118,23 +147,20 @@ namespace MyCalc
 
         private void ZnakMinusButton_Click_(object sender, RoutedEventArgs e)
         {
-
-            if (!znakmin)
-            {
-                sumText.Text = "-" + sumText.Text;
-                znakmin = true;
-            }
-            else if (znakmin)
-            {
-                sumText.Text = sumText.Text.Replace("-", "");
-                znakmin = false;
-            }
+            if(sumText.Text != "0")
+                if (sumText.Text[0] == '-')
+                    sumText.Text = sumText.Text.Remove(0, 1);
+                else
+                    sumText.Text = "-" + sumText.Text;
         }
 
         private void NumEquaButton_Click(object sender, RoutedEventArgs e)
         {
-            numText.Text += sumText.Text + (sender as Button).Content;
+            
+            if (numText.Text.IndexOf("=") == -1)
+                numText.Text += sumText.Text + (sender as Button).Content;
             CalculateSum();
+            znak = true;
         }
 
         private void DelButton_Click(object sender, RoutedEventArgs e)
@@ -151,7 +177,8 @@ namespace MyCalc
         private void ClearButton_Click(object sender, RoutedEventArgs e)
         {
             sumText.Text = "0";
-            numText.Text = "0";
+            numText.Text = "";
+            a = 0;
         }
     }
 }
